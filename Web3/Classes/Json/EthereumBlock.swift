eee3d02b1f90b5d2aca2d84eb298c2732ddc678e
Koray Koska <koray@koska.at>
2017-12-31 15:39:13 +0100

    Add more web3 objects

--- Web3/Classes/Json/EthereumBlock.swift
+++ Web3/Classes/Json/EthereumBlock.swift
@@ -13,61 +13,61 @@ import Foundation
 public struct EthereumBlock: Codable {
 
     /// The block number. nil when its a pending block.
-    let number: String?
+    public let number: String?
 
     /// 32 Bytes - hash of the block. nil when its a pending block.
-    let hash: String?
+    public let hash: String?
 
     /// 32 Bytes - hash of the parent block.
-    let parentHash: String
+    public let parentHash: String
 
     /// 8 Bytes - hash of the generated proof-of-work. nil when its a pending block.
-    let nonce: String?
+    public let nonce: String?
 
     /// 32 Bytes - SHA3 of the uncles data in the block.
-    let sha3Uncles: String
+    public let sha3Uncles: String
 
     /// 256 Bytes - the bloom filter for the logs of the block. null when its a pending block.
-    let logsBloom: String?
+    public let logsBloom: String?
 
     /// 32 Bytes - the root of the transaction trie of the block.
-    let transactionsRoot: String
+    public let transactionsRoot: String
 
     /// 32 Bytes - the root of the final state trie of the block.
-    let stateRoot: String
+    public let stateRoot: String
 
     /// 32 Bytes - the root of the receipts trie of the block.
-    let receiptsRoot: String
+    public let receiptsRoot: String
 
     /// 20 Bytes - the address of the beneficiary to whom the mining rewards were given.
-    let miner: String
+    public let miner: String
 
     /// Integer of the difficulty for this block.
-    let difficulty: String
+    public let difficulty: String
 
     /// Integer of the total difficulty of the chain until this block.
-    let totalDifficulty: String
+    public let totalDifficulty: String
 
     /// The "extra data" field of this block.
-    let extraData: String
+    public let extraData: String
 
     /// Integer the size of this block in bytes.
-    let size: String
+    public let size: String
 
     /// The maximum gas allowed in this block.
-    let gasLimit: String
+    public let gasLimit: String
 
     /// The total used gas by all transactions in this block.
-    let gasUsed: String
+    public let gasUsed: String
 
     /// The unix timestamp for when the block was collated.
-    let timestamp: String
+    public let timestamp: String
 
     /// Array of transaction objects, or 32 Bytes transaction hashes depending on the last given parameter.
-    let transactions: [Transaction]
+    public let transactions: [Transaction]
 
     /// Array of uncle hashes.
-    let uncles: [String]
+    public let uncles: [String]
 
     /**
      * Represents a transaction as either a hash or an object.
@@ -75,16 +75,26 @@ public struct EthereumBlock: Codable {
     public struct Transaction: Codable {
 
         /// The transaction as an object
-        let object: EthereumTransaction?
+        public let object: EthereumTransaction?
 
         /// The transaction as an hash
-        let hash: String?
+        public let hash: String?
 
+        /**
+         * Initialize this Transaction as an object.
+         *
+         * - parameter object: The Transaction as an object.
+         */
         public init(object: EthereumTransaction) {
             self.object = object
             self.hash = nil
         }
 
+        /**
+         * Initialize this Transaction as an hash.
+         *
+         * - parameter hash: The transaction hash.
+         */
         public init(hash: String) {
             self.hash = hash
             self.object = nil
