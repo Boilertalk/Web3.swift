55719491ceba3f4d814b714b998fce2b7ebee62c
Koray Koska <koray@koska.at>
2018-02-05 04:09:18 +0100

    Add encoding, decoding tests for big arrays w/ big items

--- Example/Tests/RLP/RLPDecoderTests.swift
+++ Example/Tests/RLP/RLPDecoderTests.swift
@@ -105,14 +105,21 @@ class RLPDecoderTests: QuickSpec {
                         return
                     }
 
-                     let i = try? decoder.decode(hex)
-                     expect(i).toNot(beNil())
-                     guard let item = i else {
+                    let i = try? decoder.decode(hex)
+                    expect(i).toNot(beNil())
+                    guard let item = i else {
                         return
-                     }
+                    }
 
-                     print("****")
-                     print(item.description)
+                    expect(item.array?[safe: 0]?.bytes) == []
+                    expect(item.array?[safe: 1]?.bytes) == [0x4, 0xe3, 0xb2, 0x92, 0x0]
+                    expect(item.array?[safe: 2]?.bytes) == [0x52, 0x8]
+                    expect(item.array?[safe: 3]?.bytes) == [0x30, 0x11, 0xf9, 0xa9, 0x5f, 0xe3, 0x5, 0x85, 0xec, 0x5b, 0x3a, 0x55, 0x5a, 0x62, 0xa5, 0x1f, 0xab, 0x94, 0x1b, 0x16]
+                    expect(item.array?[safe: 4]?.bytes) == [0x1, 0x38, 0x40, 0xe, 0xca, 0x36, 0x4a, 0x0, 0x0]
+                    expect(item.array?[safe: 5]?.bytes) == []
+                    expect(item.array?[safe: 6]?.bytes) == [0x26]
+                    expect(item.array?[safe: 7]?.bytes) == [0x63, 0xb2, 0xed, 0xbb, 0xa0, 0x5d, 0x7b, 0x2e, 0x26, 0xd9, 0x71, 0x74, 0x55, 0x34, 0x78, 0x72, 0x4b, 0x9c, 0x30, 0x53, 0x23, 0xa6, 0x7f, 0xee, 0x43, 0xfd, 0x33, 0x3a, 0x1e, 0x33, 0x6f, 0x6]
+                    expect(item.array?[safe: 8]?.bytes) == [0x38, 0x98, 0x74, 0x85, 0x8c, 0x39, 0xfd, 0xdf, 0x54, 0x37, 0x22, 0xd, 0x90, 0x53, 0x1c, 0x64, 0x9f, 0xd, 0xa5, 0x92, 0x40, 0x3d, 0xf0, 0xc1, 0x91, 0x5c, 0xb0, 0xf7, 0x20, 0x53, 0x5e, 0xa]
                 }
             }
 
@@ -174,6 +181,30 @@ class RLPDecoderTests: QuickSpec {
                     expect(item.array?[safe: 2]?.array?[safe: 0]?.array?.count) == 0
                     expect(item.array?[safe: 2]?.array?[safe: 1]?.array?.count) == 1
                     expect(item.array?[safe: 2]?.array?[safe: 1]?.array?[safe: 0]?.array?.count) == 0
+                }
+
+                it("should be an array of long latin strings") {
+                    let str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                    let strBytes = str.makeBytes()
+
+                    var rlp: [UInt8] = [0xf8, 0x74]
+                    for _ in 0..<2 {
+                        rlp.append(0xb8)
+                        rlp.append(0x38)
+
+                        for b in strBytes {
+                            rlp.append(b)
+                        }
+                    }
+
+                    let item = try? decoder.decode(rlp)
+                    expect(item).toNot(beNil())
+
+                    expect(item?.array?[safe: 0]?.bytes) == strBytes
+                    expect(item?.array?[safe: 0]?.string) == str
+
+                    expect(item?.array?[safe: 1]?.bytes) == strBytes
+                    expect(item?.array?[safe: 1]?.string) == str
                 }
             }
         }
