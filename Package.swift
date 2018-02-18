7ea3819d5fc92a70cbc909ebc73e172566f8bd6a
Koray Koska <koray@koska.at>
2018-02-18 02:28:46 +0100

    Restructure tests folder to support spm on linux

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
             name: "Web3Tests",
             dependencies: ["Web3", "Quick", "Nimble"],
             path: "Example/Tests",
-            exclude: ["InternalTests"],
-            sources: ["."]),
+            exclude: ["Web3/InternalTests"],
+            sources: ["Web3"]),
     ]
 )
