17f9fd21929f8056d78f772edfef40fdd51bc500
Koray Koska <koray@koska.at>
2018-02-18 16:34:06 +0100

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,16 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Web3",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Web3",
             targets: ["Web3"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
+        // Core dependencies
         .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.1"),
@@ -23,8 +21,6 @@ let package = Package(
         .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.3")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VaporBytes",
             dependencies: ["Bits"],
