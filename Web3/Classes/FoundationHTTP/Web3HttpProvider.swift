1cbfe56181f2b9ab69b68c3b4dcb9fb9fd6dd73c
Ybrin <koray@koska.at>
2018-03-11 23:05:05 +0100

    Make http rpc interface concurrent. Fix #5

--- Web3/Classes/FoundationHTTP/Web3HttpProvider.swift
+++ Web3/Classes/FoundationHTTP/Web3HttpProvider.swift
@@ -32,51 +32,53 @@ public struct Web3HttpProvider: Web3Provider {
     }
 
     public func send<Params, Result>(request: RPCRequest<Params>, response: @escaping Web3ResponseCompletion<Result>) {
-        guard let body = try? encoder.encode(request) else {
-            let err = Web3Response<Result>(status: .requestFailed)
-            response(err)
-            return
-        }
-
-        guard let url = URL(string: rpcURL) else {
-            let err = Web3Response<Result>(status: .requestFailed)
-            response(err)
-            return
-        }
-
-        var req = URLRequest(url: url)
-        req.httpMethod = "POST"
-        req.httpBody = body
-        for (k, v) in type(of: self).headers {
-            req.addValue(v, forHTTPHeaderField: k)
-        }
-
-        let task = session.dataTask(with: req) { data, urlResponse, error in
-            guard let urlResponse = urlResponse as? HTTPURLResponse, let data = data, error == nil else {
-                let err = Web3Response<Result>(status: .serverError)
+        queue.async {
+            guard let body = try? self.encoder.encode(request) else {
+                let err = Web3Response<Result>(status: .requestFailed)
                 response(err)
                 return
             }
 
-            let status = urlResponse.statusCode
-            guard status >= 200 && status < 300 else {
-                // This is a non typical rpc error response and should be considered a server error.
-                let err = Web3Response<Result>(status: .serverError)
+            guard let url = URL(string: self.rpcURL) else {
+                let err = Web3Response<Result>(status: .requestFailed)
                 response(err)
                 return
             }
 
-            guard let rpcResponse = try? self.decoder.decode(RPCResponse<Result>.self, from: data) else {
-                // We don't have the response we expected...
-                let err = Web3Response<Result>(status: .serverError)
-                response(err)
-                return
+            var req = URLRequest(url: url)
+            req.httpMethod = "POST"
+            req.httpBody = body
+            for (k, v) in type(of: self).headers {
+                req.addValue(v, forHTTPHeaderField: k)
             }
 
-            // We got the Result object
-            let res = Web3Response(status: .ok, rpcResponse: rpcResponse)
-            response(res)
+            let task = self.session.dataTask(with: req) { data, urlResponse, error in
+                guard let urlResponse = urlResponse as? HTTPURLResponse, let data = data, error == nil else {
+                    let err = Web3Response<Result>(status: .serverError)
+                    response(err)
+                    return
+                }
+
+                let status = urlResponse.statusCode
+                guard status >= 200 && status < 300 else {
+                    // This is a non typical rpc error response and should be considered a server error.
+                    let err = Web3Response<Result>(status: .serverError)
+                    response(err)
+                    return
+                }
+
+                guard let rpcResponse = try? self.decoder.decode(RPCResponse<Result>.self, from: data) else {
+                    // We don't have the response we expected...
+                    let err = Web3Response<Result>(status: .serverError)
+                    response(err)
+                    return
+                }
+
+                // We got the Result object
+                let res = Web3Response(status: .ok, rpcResponse: rpcResponse)
+                response(res)
+            }
+            task.resume()
         }
-        task.resume()
     }
 }
