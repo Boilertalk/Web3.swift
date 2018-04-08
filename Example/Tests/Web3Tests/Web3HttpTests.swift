4480939bad23cbacb9952041c653d8016cd203d1
Koray Koska <koray@koska.at>
2018-04-09 01:48:30 +0200

    Add eth call tests

--- Example/Tests/Web3Tests/Web3HttpTests.swift
+++ Example/Tests/Web3Tests/Web3HttpTests.swift
@@ -420,6 +420,39 @@ class Web3HttpTests: QuickSpec {
                     }
                 }
             }
+
+            context("eth call") {
+                waitUntil { done in
+                    firstly {
+                        Promise { seal in
+                            let call = try EthereumCall(
+                                from: nil,
+                                to: EthereumAddress(hex: "0x2e704bf506b96adac7ad1df0db461344146a4657", eip55: false),
+                                gas: nil,
+                                gasPrice: nil,
+                                value: nil,
+                                data: EthereumData(
+                                    ethereumValue: "0xaa1e84de000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000046461766500000000000000000000000000000000000000000000000000000000"
+                                )
+                            )
+                            seal.resolve(call, nil)
+                        }
+                    }.then { call in
+                        web3.eth.call(call: call, block: .latest)
+                    }.done { data in
+                        let expectedData: EthereumData = try .string("0x5e2393c41c2785095aa424cf3e033319468b6dcebda65e61606ee2ae2a198a87")
+                        it("should be the expected data") {
+                            expect(data) == expectedData
+                        }
+                        done()
+                    }.catch { error in
+                        it("should not fail") {
+                            expect(false) == true
+                        }
+                        done()
+                    }
+                }
+            }
         }
     }
 }
