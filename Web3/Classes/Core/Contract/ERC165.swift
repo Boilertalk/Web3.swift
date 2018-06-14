a08456e3d5552588d6f819a6c2ed6a8f19c58c58
Josh Pyles <joshpyles@gmail.com>
2018-06-13 17:53:47 -0700

    Add ABI and Contract modules to Web3.swift
    An interface for interacting with Solidity contracts similar to web3.js

---
+++ Web3/Classes/Core/Contract/ERC165.swift
@@ -0,0 +1,29 @@
+//
+//  ERC165.swift
+//  Web3
+//
+//  Created by Josh Pyles on 6/19/18.
+//
+
+import Foundation
+
+/// ERC165
+public protocol ERC165Contract: EthereumContract {
+    func supportsInterface(interface: String) -> SolidityInvocation
+}
+
+public extension ERC165Contract {
+    
+    /// Determine if a contract supports the given interface
+    ///
+    /// - Parameter interface: first 4 bytes of keccak hash of the interface, expressed as a string (ex: "0x01ffc9a7")
+    /// - Returns: Invocation for this method with the interface
+    public func supportsInterface(interface: String) -> SolidityInvocation {
+        let inputs = [SolidityFunctionParameter(name: "_interface", type: .bytes(length: 4))]
+        let outputs = [SolidityFunctionParameter(name: "_supportsInterface", type: .bool)]
+        let method = SolidityConstantFunction(name: "supportsInterface", inputs: inputs, outputs: outputs, handler: self)
+        let interfaceData = Data(hex: interface)
+        return method.invoke(interfaceData)
+    }
+    
+}
