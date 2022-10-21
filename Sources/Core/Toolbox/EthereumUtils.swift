a05a112821f99174080ea40ef30ecec71e8e8a5e
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2022-10-21 14:26:28 +0100

    chore: reformat to match style

--- Sources/Core/Toolbox/EthereumUtils.swift
+++ Sources/Core/Toolbox/EthereumUtils.swift
@@ -1,37 +1,33 @@
-
 import Foundation
 
-
-
 public struct EthereumUtils {
-    
+
     enum Error: Swift.Error {
-    case saltNot32BytesError
-    case initCodeHashNot32BytesError
+        case saltNot32BytesError
+        case initCodeHashNot32BytesError
     }
-    
-    public static func getCreate2Address(from: String, salt: [UInt8], initCodeHash: [UInt8]) throws -> String {
+
+    public static func getCreate2Address(from: EthereumAddress, salt: [UInt8], initCodeHash: [UInt8]) throws -> String {
         if salt.count != 32 {
             throw Error.saltNot32BytesError
         }
         if initCodeHash.count != 32 {
             throw Error.initCodeHashNot32BytesError
         }
-        
+
         var concat: [UInt8] = [0xff]
-        concat.append(contentsOf: try EthereumAddress(hex: from, eip55: false).rawAddress)
+        concat.append(contentsOf: from.rawAddress)
         concat.append(contentsOf: salt)
         concat.append(contentsOf: initCodeHash)
-        
+
         let hash = concat.sha3(.keccak256)
         let hexSlice = Array(hash[12...])
-        
+
         return try EthereumAddress(hex: hexSlice.toHexString(), eip55: false).hex(eip55: true)
     }
-    
-    public static func getCreate2Address(from: String, salt: [UInt8], initCode: [UInt8]) throws -> String {
-        let initCodeHash: [UInt8] = initCode.sha3(.keccak256)
+
+    public static func getCreate2Address(from: EthereumAddress, salt: [UInt8], initCode: [UInt8]) throws -> String {
+        let initCodeHash = initCode.sha3(.keccak256)
         return try getCreate2Address(from: from, salt: salt, initCodeHash: initCodeHash)
     }
-    
 }
