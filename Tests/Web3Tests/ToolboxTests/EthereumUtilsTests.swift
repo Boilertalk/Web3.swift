9e463d451e12a72d6e6a19f1c1adea30312f5b2a
FlorianSAW <florian@vitracash.com>
2022-10-30 16:07:36 +0000

    Update EthereumUtisl tests to changed return type

--- Tests/Web3Tests/ToolboxTests/EthereumUtilsTests.swift
+++ Tests/Web3Tests/ToolboxTests/EthereumUtilsTests.swift
@@ -24,43 +24,43 @@ final class EthereumUtilsTests: QuickSpec {
                 it("should be 0x4D1A2e2bB4F88F0250f26Ffff098B0b30B26BF38") {
                    let result = try EthereumUtils.getCreate2Address(
                     from: address0, salt: salt0, initCodeHash: init_code_hash0)
-                   expect(result) == "0x4D1A2e2bB4F88F0250f26Ffff098B0b30B26BF38"
+                    try expect(result) == EthereumAddress(hex: "0x4D1A2e2bB4F88F0250f26Ffff098B0b30B26BF38", eip55: false)
                 }
 
                 it("should be 0xB928f69Bb1D91Cd65274e3c79d8986362984fDA3") {
                     let result = try EthereumUtils.getCreate2Address(
                         from: address1, salt: salt0, initCodeHash: init_code_hash0)
-                    expect(result) == "0xB928f69Bb1D91Cd65274e3c79d8986362984fDA3"
+                    try expect(result) == EthereumAddress(hex: "0xB928f69Bb1D91Cd65274e3c79d8986362984fDA3", eip55: false)
                 }
 
                 it("should be 0xD04116cDd17beBE565EB2422F2497E06cC1C9833") {
                     let result = try EthereumUtils.getCreate2Address(
                         from: address1, salt: salt1, initCodeHash: init_code_hash0)
-                    expect(result) == "0xD04116cDd17beBE565EB2422F2497E06cC1C9833"
+                    try expect(result) == EthereumAddress(hex: "0xD04116cDd17beBE565EB2422F2497E06cC1C9833", eip55: false)
                 }
 
                 it("should be 0x70f2b2914A2a4b783FaEFb75f459A580616Fcb5e") {
                     let result = try EthereumUtils.getCreate2Address(
                         from: address0, salt: salt0, initCodeHash: init_code_hash1)
-                    expect(result) == "0x70f2b2914A2a4b783FaEFb75f459A580616Fcb5e"
+                    try expect(result) == EthereumAddress(hex: "0x70f2b2914A2a4b783FaEFb75f459A580616Fcb5e", eip55: false)
                 }
 
                 it("should be 0x60f3f640a8508fC6a86d45DF051962668E1e8AC7") {
                     let result = try EthereumUtils.getCreate2Address(
                         from: address2, salt: salt2, initCodeHash: init_code_hash1)
-                    expect(result) == "0x60f3f640a8508fC6a86d45DF051962668E1e8AC7"
+                    try expect(result) == EthereumAddress(hex: "0x60f3f640a8508fC6a86d45DF051962668E1e8AC7", eip55: false)
                 }
 
                 it("should be 0x1d8bfDC5D46DC4f61D6b6115972536eBE6A8854C") {
                     let result = try EthereumUtils.getCreate2Address(
                         from: address2, salt: salt2, initCode: init_code0)
-                    expect(result) == "0x1d8bfDC5D46DC4f61D6b6115972536eBE6A8854C"
+                    try expect(result) == EthereumAddress(hex: "0x1d8bfDC5D46DC4f61D6b6115972536eBE6A8854C", eip55: false)
                 }
 
                 it("should be 0xE33C0C7F7df4809055C3ebA6c09CFe4BaF1BD9e0") {
                     let result = try EthereumUtils.getCreate2Address(
                         from: address0, salt: salt0, initCode: init_code1)
-                    expect(result) == "0xE33C0C7F7df4809055C3ebA6c09CFe4BaF1BD9e0"
+                    try expect(result) == EthereumAddress(hex: "0xE33C0C7F7df4809055C3ebA6c09CFe4BaF1BD9e0", eip55: false)
                 }
             }
         }
