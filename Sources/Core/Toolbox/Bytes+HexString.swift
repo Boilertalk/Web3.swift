561c1ff2e12c89153a17044c9a3ba4efbec588a8
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2022-10-30 21:43:46 +0000

    feat: performance optimizations for string manipulation (#139)

--- Sources/Core/Toolbox/Bytes+HexString.swift
+++ Sources/Core/Toolbox/Bytes+HexString.swift
@@ -7,35 +7,45 @@
 
 import Foundation
 
+fileprivate let hexMapping = Array("0123456789abcdef")
+
 extension Array where Element == Byte {
 
     func hexString(prefix: Bool) -> String {
-        var str = prefix ? "0x" : ""
+        var charArray: [String.Element] = [String.Element](repeating: "0", count: self.count * 2)
 
-        for b in self {
-            str += String(format: "%02x", b)
+        for i in 0..<self.count {
+            charArray[i * 2] = hexMapping[Int(self[i]) / 16]
+            charArray[(i * 2) + 1] = hexMapping[Int(self[i]) % 16]
         }
 
-        return str
+        if prefix {
+            return "0x\(String(charArray))"
+        } else {
+            return String(charArray)
+        }
     }
 
     func quantityHexString(prefix: Bool) -> String {
-        var str = prefix ? "0x" : ""
-
         // Remove leading zero bytes
         let bytes = self.trimLeadingZeros()
 
-        if bytes.count > 0 {
-            // If there is one leading zero (4 bit) left, this one removes it
-            str += String(bytes[0], radix: 16)
-
-            for i in 1..<bytes.count {
-                str += String(format: "%02x", bytes[i])
-            }
+        var hex = ""
+        if bytes.count == 0 {
+            hex = "0"
         } else {
-            str += "0"
+            hex = bytes.hexString(prefix: false)
+
+            // If there is one leading zero (4 bit) left, this one removes it
+            if hex.starts(with: "0") {
+                hex = String(hex[hex.index(hex.startIndex, offsetBy: 1)...])
+            }
         }
 
-        return str
+        if prefix {
+            return "0x\(hex)"
+        } else {
+            return hex
+        }
     }
 }
