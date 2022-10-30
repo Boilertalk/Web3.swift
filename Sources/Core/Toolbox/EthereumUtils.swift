4e9adf6472dd0e3996ba9c4f9b9e71fe767bf27a
Florian Winkler <florian@vitracash.com>
2022-10-30 17:19:51 +0100

    Change return type of getCreate2Address to EthereumAddress (#138)
    * Change return type of getCreate2Address to EthereumAddress
    
    * Update EthereumUtisl tests to changed return type

--- Sources/Core/Toolbox/EthereumUtils.swift
+++ Sources/Core/Toolbox/EthereumUtils.swift
@@ -7,7 +7,7 @@ public struct EthereumUtils {
         case initCodeHashNot32BytesError
     }
 
-    public static func getCreate2Address(from: EthereumAddress, salt: [UInt8], initCodeHash: [UInt8]) throws -> String {
+    public static func getCreate2Address(from: EthereumAddress, salt: [UInt8], initCodeHash: [UInt8]) throws -> EthereumAddress {
         if salt.count != 32 {
             throw Error.saltNot32BytesError
         }
@@ -23,10 +23,10 @@ public struct EthereumUtils {
         let hash = concat.sha3(.keccak256)
         let hexSlice = Array(hash[12...])
 
-        return try EthereumAddress(hex: hexSlice.toHexString(), eip55: false).hex(eip55: true)
+        return try EthereumAddress(hex: hexSlice.toHexString(), eip55: false)
     }
 
-    public static func getCreate2Address(from: EthereumAddress, salt: [UInt8], initCode: [UInt8]) throws -> String {
+    public static func getCreate2Address(from: EthereumAddress, salt: [UInt8], initCode: [UInt8]) throws -> EthereumAddress {
         let initCodeHash = initCode.sha3(.keccak256)
         return try getCreate2Address(from: from, salt: salt, initCodeHash: initCodeHash)
     }
