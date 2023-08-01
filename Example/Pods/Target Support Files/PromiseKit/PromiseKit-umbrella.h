c64ea4ab0eee7111c24485382ffe1723adef3442
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-08-01 17:27:07 +0100

    Remove PromiseKit extensions dependency

--- Example/Pods/Target Support Files/PromiseKit/PromiseKit-umbrella.h
+++ Example/Pods/Target Support Files/PromiseKit/PromiseKit-umbrella.h
@@ -13,13 +13,6 @@
 #import "fwd.h"
 #import "AnyPromise.h"
 #import "PromiseKit.h"
-#import "NSURLSession+AnyPromise.h"
-#import "NSTask+AnyPromise.h"
-#import "NSNotificationCenter+AnyPromise.h"
-#import "PMKFoundation.h"
-#import "PMKUIKit.h"
-#import "UIView+AnyPromise.h"
-#import "UIViewController+AnyPromise.h"
 
 FOUNDATION_EXPORT double PromiseKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
