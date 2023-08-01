474a11315c2f55e95358120e8cc1dee7196118fb
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-08-01 17:26:25 +0100

    Add transaction signing and first tests

--- Example/Tests/TransactionTests/EthereumPrivateKeyTests.swift
+++ Example/Tests/TransactionTests/EthereumPrivateKeyTests.swift
@@ -9,6 +9,7 @@
 import Quick
 import Nimble
 @testable import Web3
+import BigInt
 
 class EthereumPrivateKeyTests: QuickSpec {
 
@@ -64,6 +65,29 @@ class EthereumPrivateKeyTests: QuickSpec {
                         0x0c, 0xce, 0xd0, 0x8f, 0x6b, 0x2c, 0x56, 0x76, 0x75, 0x08, 0xf3, 0xe7, 0x97, 0x6d, 0x5b, 0xcc,
                         0xd5, 0x2c, 0x91, 0xbf, 0x59, 0x77, 0xbb, 0x5d, 0x95, 0x43, 0x82, 0x67, 0x52, 0x26, 0x79, 0x18
                     ]
+                }
+            }
+
+            context("message signing") {
+
+                it("should be a valid signed message") {
+                    let p = try? EthereumPrivateKey(
+                        hexPrivateKey: "0x94eca03b4541a0eb0d173e321b6f960d08cfe4c5a75fa00ebe0a3d283c609c3a"
+                    )
+                    let t = p?.address
+                    expect(t).toNot(beNil())
+
+                    guard let to = t, let privateKey = p else {
+                        return
+                    }
+
+                    print("****")
+
+                    var tx = Transaction(nonce: 0, gasPrice: 21000000000, gasLimit: 21000, to: to, value: BigUInt(10).power(18), chainId: 3)
+
+                    let expectedTransaction = "f86c808504e3b2920082520894867aeeeed428ed9ba7f97fc7e16f16dfcf02f375880de0b6b3a76400008029a099060c9146c68716da3a79533866dc941a03b171911d675f518c97a73882f7a6a0019167adb26b602501c954e7793e798407836f524b9778f5be6ebece5fc998c6"
+
+                    expect(try? RLPEncoder().encode(tx.sign(with: privateKey).rlp()).toHexString()) == expectedTransaction
                 }
             }
         }
--- Web3/Classes/Transaction/Transaction.swift
+++ Web3/Classes/Transaction/Transaction.swift
@@ -11,6 +11,8 @@ import VaporBytes
 
 public struct Transaction {
 
+    // MARK: - Properties
+
     /// The number of transactions made prior to this one
     public let nonce: UInt
 
@@ -30,7 +32,7 @@ public struct Transaction {
     public let data: Bytes
 
     /// EC signature parameter v
-    public let v: BigUInt
+    public let v: UInt
 
     /// EC signature parameter r
     public let r: BigUInt
@@ -40,6 +42,8 @@ public struct Transaction {
 
     /// EIP 155 chainId. Mainnet: 1
     public let chainId: UInt
+
+    // MARK: - Initialization
 
     /**
      * Initializes a new instance of `Transaction` with the given values.
@@ -53,7 +57,9 @@ public struct Transaction {
      * - parameter v: EC signature parameter v. Defaults to 0.
      * - parameter r: EC signature parameter r. Defaults to 0.
      * - parameter s: EC recovery ID. Defaults to 0.
-     * - parameter chainId: The chainId as described in EIP155. Mainnent: 1
+     * - parameter chainId: The chainId as described in EIP155. Mainnent: 1.
+     *                      If set to 0 and v doesn't contain a chainId,
+     *                      old style transactions are assumed.
      */
     public init(
         nonce: UInt,
@@ -62,7 +68,7 @@ public struct Transaction {
         to: EthereumAddress,
         value: BigUInt,
         data: Bytes = [],
-        v: BigUInt = 0,
+        v: UInt = 0,
         r: BigUInt = 0,
         s: BigUInt = 0,
         chainId: UInt
@@ -76,6 +82,137 @@ public struct Transaction {
         self.v = v
         self.r = r
         self.s = s
-        self.chainId = chainId
+
+        if chainId == 0 && v >= 37 {
+            if v % 2 == 0 {
+                self.chainId = (v - 36) / 2
+            } else {
+                self.chainId = (v - 35) / 2
+            }
+        } else {
+            self.chainId = chainId
+        }
+    }
+
+    // MARK: - Convenient functions
+
+    /**
+     * Signs this transaction with the given private key and discards old signatures if present.
+     *
+     * - parameter privateKey: The private key for the new signature.
+     */
+    @discardableResult
+    public mutating func sign(with privateKey: EthereumPrivateKey) throws -> Transaction {
+        let rawRlp = try RLPEncoder().encode(rlp(forSigning: true))
+        let signature = try privateKey.sign(message: rawRlp)
+
+        let v: UInt
+        if self.chainId == 0 {
+            v = signature.v + 27
+        } else {
+            v = signature.v + 27 + (chainId * 2 + 8)
+        }
+
+        let r = BigUInt(bytes: signature.r)
+        let s = BigUInt(bytes: signature.s)
+
+        self = Transaction(
+            nonce: self.nonce,
+            gasPrice: self.gasPrice,
+            gasLimit: self.gasLimit,
+            to: self.to,
+            value: self.value,
+            data: self.data,
+            v: v,
+            r: r,
+            s: s,
+            chainId: self.chainId
+        )
+
+        return self
+    }
+
+    // MARK: - Errors
+
+    public enum Error: Swift.Error {
+
+        case transactionInvalid
+        case rlpItemInvalid
+    }
+}
+
+extension Transaction: RLPItemConvertible {
+
+    public init(rlp: RLPItem) throws {
+        guard let array = rlp.array, array.count == 9 else {
+            throw Error.rlpItemInvalid
+        }
+        guard let nonce = array[0].uint, let gasPrice = array[1].uint, let gasLimit = array[2].uint,
+            let toBytes = array[3].bytes, let to = try? EthereumAddress(rawAddress: toBytes),
+            let value = array[4].bigUInt, let data = array[5].bytes, let v = array[6].uint,
+            let r = array[7].bigUInt, let s = array[8].bigUInt else {
+                throw Error.rlpItemInvalid
+        }
+
+        self.init(
+            nonce: nonce,
+            gasPrice: gasPrice,
+            gasLimit: gasLimit,
+            to: to,
+            value: value,
+            data: data,
+            v: v,
+            r: r,
+            s: s,
+            chainId: 0
+        )
+    }
+
+    public func rlp() -> RLPItem {
+        return rlp(forSigning: false)
+    }
+
+    public func rlp(forSigning: Bool) -> RLPItem {
+        let item: RLPItem
+        if forSigning {
+            if chainId == 0 {
+                item = [
+                    .uint(nonce),
+                    .uint(gasPrice),
+                    .uint(gasLimit),
+                    .bytes(to.rawAddress),
+                    .bigUInt(value),
+                    .bytes(data)
+                ]
+            } else {
+                item = [
+                    .uint(nonce),
+                    .uint(gasPrice),
+                    .uint(gasLimit),
+                    .bytes(to.rawAddress),
+                    .bigUInt(value),
+                    .bytes(data),
+
+                    // EIP 155: For signing and recovering: replace v with chainId and r and s with 0
+                    .uint(chainId),
+                    .bigUInt(0),
+                    .bigUInt(0)
+                ]
+            }
+        } else {
+            item = [
+                .uint(nonce),
+                .uint(gasPrice),
+                .uint(gasLimit),
+                .bytes(to.rawAddress),
+                .bigUInt(value),
+                .bytes(data),
+                .uint(v),
+                .bigUInt(r),
+                .bigUInt(s)
+            ]
+        }
+
+        return item
     }
 }
