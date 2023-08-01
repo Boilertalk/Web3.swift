ad74393f761dd8bcf07b6e2f791e89297375d1a2
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-08-01 17:26:31 +0100

    Restructure EthereumCall

---
+++ Web3/Classes/Json/EthereumCall.swift
@@ -0,0 +1,127 @@
+//
+//  EthereumCallParams.swift
+//  Web3
+//
+//  Created by Koray Koska on 11.02.18.
+//
+
+import Foundation
+
+public struct EthereumCall: Codable {
+
+    /// The address the transaction is sent from.
+    public let from: EthereumAddress?
+
+    /// The address the transaction is directed to.
+    public let to: EthereumAddress
+
+    /// Integer of the gas provided for the transaction execution.
+    /// `eth_call` consumes zero gas, but this parameter may be needed by some executions.
+    public let gas: EthereumQuantity?
+
+    /// Integer of the gasPrice used for each paid gas
+    public let gasPrice: EthereumQuantity?
+
+    /// Integer of the value send with this transaction
+    public let value: EthereumQuantity?
+
+    /// Hash of the method signature and encoded parameters.
+    /// For details see https://github.com/ethereum/wiki/wiki/Ethereum-Contract-ABI
+    public let data: EthereumData?
+
+    public init(
+        from: EthereumAddress? = nil,
+        to: EthereumAddress,
+        gas: EthereumQuantity? = nil,
+        gasPrice: EthereumQuantity? = nil,
+        value: EthereumQuantity? = nil,
+        data: EthereumData? = nil
+        ) {
+        self.from = from
+        self.to = to
+        self.gas = gas
+        self.gasPrice = gasPrice
+        self.value = value
+        self.data = data
+    }
+}
+
+public struct EthereumCallParams: Codable {
+
+    /// The actual call parameters
+    public let call: EthereumCall
+
+    /// The address the transaction is sent from.
+    public var from: EthereumAddress? {
+        return call.from
+    }
+
+    /// The address the transaction is directed to.
+    public var to: EthereumAddress {
+        return call.to
+    }
+
+    /// Integer of the gas provided for the transaction execution.
+    /// `eth_call` consumes zero gas, but this parameter may be needed by some executions.
+    public var gas: EthereumQuantity? {
+        return call.gas
+    }
+
+    /// Integer of the gasPrice used for each paid gas
+    public var gasPrice: EthereumQuantity? {
+        return call.gasPrice
+    }
+
+    /// Integer of the value send with this transaction
+    public var value: EthereumQuantity? {
+        return call.value
+    }
+
+    /// Hash of the method signature and encoded parameters.
+    /// For details see https://github.com/ethereum/wiki/wiki/Ethereum-Contract-ABI
+    public var data: EthereumData? {
+        return call.data
+    }
+
+    /// Integer block number, or the string "latest", "earliest" or "pending"
+    public let block: EthereumQuantityTag
+
+    public init(
+        call: EthereumCall,
+        block: EthereumQuantityTag
+    ) {
+        self.call = call
+        self.block = block
+    }
+
+    public init(
+        from: EthereumAddress? = nil,
+        to: EthereumAddress,
+        gas: EthereumQuantity? = nil,
+        gasPrice: EthereumQuantity? = nil,
+        value: EthereumQuantity? = nil,
+        data: EthereumData? = nil,
+        block: EthereumQuantityTag
+        ) {
+        let call = EthereumCall(from: from, to: to, gas: gas, gasPrice: gasPrice, value: value, data: data)
+        self.init(call: call, block: block)
+    }
+
+    public init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+
+        let call = try container.decode(EthereumCall.self)
+
+        let block = try container.decode(EthereumQuantityTag.self)
+
+        self.init(call: call, block: block)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+
+        try container.encode(call)
+
+        try container.encode(block)
+    }
+}
