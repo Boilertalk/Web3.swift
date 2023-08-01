05f80219101f14767efc73ecc5c51d8b9fbbeea8
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-08-01 17:26:45 +0100

    Add Package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,41 @@
 import PackageDescription
 
 let package = Package(
-    name: "shit",
+    name: "Web3",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "shit",
-            targets: ["shit"]),
+            name: "VaporBytes",
+            targets: ["VaporBytes"]),
+        .library(
+            name: "Web3",
+            targets: ["Web3"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
+        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
+        .package(url: "https://github.com/vapor/bits.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "shit",
-            dependencies: []),
+            path: "Web3/Classes",
+            sources: ["VaporBytes"],
+            name: "VaporBytes",
+            dependencies: ["Bits"]),
+        .target(
+            path: "Web3/Classes",
+            sources: ["Core"],
+            name: "Web3",
+            dependencies: ["VaporBytes", "BigInt", "CryptoSwift", "secp256k1"]),
         .testTarget(
-            name: "shitTests",
-            dependencies: ["shit"]),
+            path: "Example/Tests",
+            exclude: ["LinuxMain.swift"],
+            sources: ".",
+            name: "Web3Tests",
+            dependencies: ["Web3"]),
     ]
 )
