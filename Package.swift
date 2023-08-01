78f31a049ab3b87115526607c9c04533a8fd1c9d
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-08-01 17:26:46 +0100

    Bump minimum version for secp256k1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
-        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
+        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.1"),
         .package(url: "https://github.com/vapor/bits.git", from: "1.1.0"),
 
         // Test dependencies
