6d11bc31e97053152910f51ef356a6b1f488af12
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-08-01 17:26:51 +0100

    Revert restructure, add SPM Tests directory

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,21 @@
+//
+//  LinuxMain.swift
+//  Web3
+//
+//  Created by Koray Koska on 16.02.18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#if os(Linux)
+
+import XCTest
+import Quick
+@testable import Web3
+
+QCKMain([
+    RLPEncoderTests.self,
+    RLPDecoderTests.self,
+    RLPItemTests.self
+])
+
+#endif
