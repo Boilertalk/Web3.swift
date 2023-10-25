606fc3aa719533dcc82e259a61a6c25e516e27d3
junhojo <charm1109@naver.com>
2023-10-25 19:13:10 +0900

    CryptoSwift
    CryptoSwift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     dependencies: [
         // Core dependencies
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1"),
         .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
         .package(url: "https://github.com/vapor/websocket-kit", .upToNextMajor(from: "2.6.1")),
