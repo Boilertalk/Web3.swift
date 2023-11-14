f2c3c863ee25cd606defd5d987c8d3f1a4f2bc15
cuhte3 <farid@getportal.co>
2023-11-14 22:36:22 +0700

    Update Package.swift
    sepc256k1 vendor changed to match evmKit dependency

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // Core dependencies
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.6.0"),
-        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7"),
+        .package(name: "secp256k1", url: "https://github.com/GigaBitcoin/secp256k1.swift.git", .exact("0.10.0")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
         .package(url: "https://github.com/vapor/websocket-kit", .upToNextMajor(from: "2.6.1")),
 
