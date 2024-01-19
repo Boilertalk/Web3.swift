d9595cafad3af384e3adcf15a2981e1b196c9ded
junhojo <charm1109@naver.com>
2024-01-19 14:51:38 +0900

    수정

--- Package.swift
+++ Package.swift
@@ -5,7 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Web3",
     platforms: [
-       .iOS(.v13),
+        .iOS(.v13),      
+        .macOS(.v10_15),
+        .watchOS(.v6),
+        .tvOS(.v13),
+        .macCatalyst(.v14),
+        .driverKit(.v20),
     ],
     products: [
         .library(
